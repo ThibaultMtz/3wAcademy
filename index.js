4b8674c05d1875a59937265664f6bef535668fda
SHATAJV <97613899+SHATAJV@users.noreply.github.com>
2023-10-19 16:11:36 +0200

    Update index.js
    change the button de menu

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let UserScore = 0;
+et UserScore = 0;
 let CpuScore = 0;
 let rounds = 0; 
 
@@ -85,3 +85,34 @@ buttons.forEach(button => {
 });
 
 console.log('Player: ' + UserScore + ' / Rival: ' + CpuScore + '.');
+const button = document.querySelector('button')
+const nav = document.querySelector('nav')
+const backdrop = document.querySelector('.backdrop')
+
+//ouvre le menu
+button.addEventListener('click', () => {
+    nav.classList.add('open')
+})
+
+//enleve le menu et revient sur la page de jeu 
+backdrop.addEventListener('click', () => {
+    nav.classList.remove('open');
+})
+
+const continuer = document.getElementById('continuerBouton');
+const recommencer = document.getElementById('recommencerBouton')
+const quitter = document.getElementById('quitterBouton')
+
+continuer.addEventListener('click', () => {
+    nav.classList.remove('open');
+})
+
+//rafraichit la page pour le joueur recommence son jeu 
+recommencer.addEventListener('click', () => {
+    location.reload();
+})
+
+quitter.addEventListener('click', () => {
+    //insérer le lien de votre page pour rediriger à l'acceuil du jeu)
+    location.href = 'https://google.com'; 
+})
